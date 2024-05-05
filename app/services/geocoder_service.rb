f305a1f88ee958d55f090ffbc9bbd746940845fc
Scott Shervington <scott@bigbadmachine.com>
2024-05-04 20:59:47 -0400

    Encapsulate response of GeocoderService

--- app/services/geocoder_service.rb
+++ app/services/geocoder_service.rb
@@ -12,7 +12,7 @@ class GeocoderService < ApplicationService
       end
       
       if geocoder&.latitude
-        return_success geocoder
+        return_success encapsulate(geocoder)
       else
         return_errors [I18n.t("errors.no_match")]
       end
@@ -25,5 +25,16 @@ class GeocoderService < ApplicationService
 
   def cache_key
     @address.parameterize
+  end
+
+  def encapsulate(geocoder)
+    # returning as OpenStruct instead of Geocoder object
+    # decouples ForecasterService from GeocoderService
+    # Allows a simple change to another geocoding api/gem
+    OpenStruct.new(
+      latitude: geocoder.latitude, 
+      longitude: geocoder.longitude, 
+      postal_code: geocoder.postal_code
+    )
   end
 end
